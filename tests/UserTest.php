a32bce1c42cca88f7dbfe82039e27edc9d93f6b6
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 19:04:32 -0700

    Write test getFirstName for UserTest.php

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -14,10 +14,20 @@
         function test_getFirstName()
         {
             //Arrange
-            
+            $first_name = 'Maggie';
+            $last_name = 'Doe';
+            $email = 'maggie@me.com';
+            $username = 'Mags123';
+            $password = '1234';
+            $activity_level = 2;
+            $id = 1;
+            $test_user = new User($first_name, $last_name, $email, $username, $password, $activity_level, $id);
+
             //Act
+            $result = $test_user->getFirstName();
 
             //Assert
+            $this->assertEquals('Maggie', $result);
         }
 
 

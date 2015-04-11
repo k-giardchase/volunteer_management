c59a8d6406e9063326c432b08eff2bc566506d73
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-11 11:00:31 -0700

    Write test setUsername

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -147,6 +147,26 @@
             $this->assertEquals('Mags123', $result);
         }
 
+        function test_setUsername()
+        {
+            //Arrange
+            $first_name = 'Maggie';
+            $last_name = 'Doe';
+            $email = 'maggie@me.com';
+            $username = 'Mags123';
+            $password = '1234';
+            $activity_level = 2;
+            $id = 1;
+            $test_user = new User($first_name, $last_name, $email, $username, $password, $activity_level, $id);
+
+            //Act
+            $test_user->setUsername('Maggie123');
+            $result = $test_user->getUsername();
+
+            //Assert
+            $this->assertEquals('Maggie123', $result);
+        }
+
 
 
     }

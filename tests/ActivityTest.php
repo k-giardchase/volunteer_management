01a61214f6a35bfba2365faad1bc3faf86c1bf51
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-11 12:37:40 -0700

    Write test for find method

--- tests/ActivityTest.php
+++ tests/ActivityTest.php
@@ -129,6 +129,26 @@
             //Assert
             $this->assertEquals([], $result);
         }
+
+        function test_find()
+        {
+            //Arrange
+            $activity_name = 'Sleeping';
+            $id = 1;
+            $test_activity = new Activity($activity_name, $id);
+            $test_activity->save();
+
+            $activity_name2 = 'Running';
+            $id2 = 2;
+            $test_activity2 = new Activity($activity_name, $id);
+            $test_activity2->save();
+
+            //Act
+            $result = Activity::find($test_activity2->getId());
+
+            //Assert
+            $this->assertEquals($test_activity2, $result);
+        }
     }
 
 ?>

b9a70d8afb706b275b52010e4ac770f39e25e610
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-11 11:12:59 -0700

    Write test getId

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -245,6 +245,25 @@
             $this->assertEquals(4, $result);
         }
 
+        function test_getId()
+        {
+            //Arrange
+            $first_name = 'Maggie';
+            $last_name = 'Doe';
+            $email = 'maggie@me.com';
+            $username = 'Mags123';
+            $password = '1234';
+            $activity_level = 2;
+            $id = 1;
+            $test_user = new User($first_name, $last_name, $email, $username, $password, $activity_level, $id);
+
+            //Act
+            $result = $test_user->getId();
+
+            //Assert
+            $this->assertEquals(1, $result);
+        }
+
 
 
     }

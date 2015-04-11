1dc3b8b5616cefe0c079a0db7de1acd7d1f20f1f
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-10 19:10:10 -0700

    Write test getLastName

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -50,6 +50,25 @@
             $this->assertEquals('Margaret', $result);
         }
 
+        function test_getLastName()
+        {
+            //Arrange
+            $first_name = 'Maggie';
+            $last_name = 'Doe';
+            $email = 'maggie@me.com';
+            $username = 'Mags123';
+            $password = '1234';
+            $activity_level = 2;
+            $id = 1;
+            $test_user = new User($first_name, $last_name, $email, $username, $password, $activity_level, $id);
+
+            //Act
+            $result = $test_user->getLastName();
+
+            //Assert
+            $this->assertEquals('Doe', $result);
+        }
+
 
 
     }

7fec64ff51aca924b96d94480e8d748453c47adb
Kyle Giard-Chase <kyle.giardchase@gmail.com>
2015-04-24 19:02:04 -0700

    Add session variable to routes to store admin_stat and volunteer id or supervisor id.

--- app/app.php
+++ app/app.php
@@ -7,7 +7,9 @@
     require_once __DIR__.'/../src/Supervisor.php';
 
     $app = new Silex\Application();
+
     $app['debug'] = true;
+
     $DB = new PDO('pgsql:host=localhost;dbname=volunteer_management');
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
@@ -17,11 +19,41 @@
     use Symfony\Component\HttpFoundation\Request;
     Request::enableHttpMethodParameterOverride();
 
+    session_start();
+     if (empty($_SESSION['volunteer_id'])) {
+         $_SESSION['volunteer_id'] = null;
+     };
+     if (empty($_SESSION['supervisor_id'])) {
+         $_SESSION['supervisor_id'] = null;
+     };
+     if (empty($_SESSION['admin_stat'])) {
+         $_SESSION['admin_stat'] = null;
+     };
+
     $app->get("/", function() use ($app) {
-        return $app['twig']->render('index.twig', array('events' => Event::getAll(), 'committees' => Committee::getAll()));
+        $volunteer = Volunteer::find($_SESSION['volunteer_id']);
+        $supervisor = Supervisor::find($_SESSION['supervisor_id']);
+
+        if($volunteer) {
+            $volunteer_admin_stat = $volunteer->getAdminStat();
+        } else {
+            $volunteer_admin_stat = null;
+        }
+        if($supervisor) {
+            $supervisor_admin_stat = $supervisor->getAdminStat();
+        } else {
+            $supervisor_admin_stat = null;
+        }
+        return $app['twig']->render('index.twig', array('events' => Event::getAll(),
+                                                        'committees' => Committee::getAll(),
+                                                        'volunteer' => $volunteer,
+                                                        'supervisor' => $supervisor,
+                                                        'supervisor_admin_stat' => $supervisor_admin_stat,
+                                                        'volunteer_admin_stat' => $volunteer_admin_stat));
     });
 
     $app->get('/supervisor-login', function() use ($app) {
+
         return $app['twig']->render('supervisor-login.twig');
     });
 
@@ -48,11 +80,16 @@
             $admin_stat = 0;
             $new_volunteer = new Volunteer($first_name, $last_name, $email, $phone, $username, $password, $admin_stat);
             $new_volunteer->save();
+            $new_volunteer_id = $new_volunteer->getId();
+            $_SESSION['volunteer_id'] = $new_volunteer_id;
         } else {
             $available = 0;
             return $app['twig']->render('create-volunteer.twig', array('available' => $available));
         }
-        return $app['twig']->render('create-volunteer-success.twig', array('volunteer' => $new_volunteer));
+        return $app['twig']->render('create-volunteer-success.twig', array('volunteer' => $new_volunteer,
+                                                                            'volunteer_id' => $_SESSION['volunteer_id'],
+                                                                            'supervisor_id' => $_SESSION['supervisor_id'],
+                                                                            'admin_stat' => $_SESSION['admin_stat']));
     });
 
     return $app;
